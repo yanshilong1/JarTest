add784860c697995dc997b3845aa576a833d9b01
yanshilong1 <1943277338@qq.com>
2018-09-07 00:24:48 +0800

    JARTest

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+import org.JarTst.SoutJar;
+
+/**
+ * @Author: yanshilong
+ * @Date: 18-9-7 上午12:14
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        String aa=SoutJar.soutTest();
+        System.out.println(aa);
+    }
+}
